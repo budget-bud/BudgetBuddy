d4186ff1d48f5b9598cd8ae64be71ae75e8d6168
AlexMilos <11dmilosa@gmail.com>
2023-11-21 23:39:09 +0100

    feat: add basic routes

---
+++ app/chat/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const NewChatPage = () => {
+  return <div>NewChatPage</div>;
+};
+
+export default NewChatPage;
---
+++ app/tracker/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const TrackerPage = () => {
+  return <div>TrackerPage</div>;
+};
+
+export default TrackerPage;
