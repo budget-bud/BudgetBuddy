ad553eb17391581fdf417fcb5252f93a1a0142ad
AlexMilos <11dmilosa@gmail.com>
2023-12-13 21:39:39 +0100

    chore: fixed height of the header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,6 @@
 export default function Header() {
   return (
-    <header className="w-full flex flex-row justify-center gap-16 bg-slate-300">
+    <header className="w-full h-10 flex flex-row justify-center gap-16 bg-slate-300">
       <h1 className="text-4xl font-bold">Header</h1>
     </header>
   );
