311c0631bdb316d9ee35fe1103b171dc84de9958
Kálmán Neszlényi <73647069+kamka427@users.noreply.github.com>
2023-12-15 02:05:40 +0100

    fix: added error messages to oauth provider requests

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -45,7 +45,7 @@ export default function Login({
     });
 
     if (error) {
-      return redirect("/login?message=Could not authenticate user");
+      return redirect("/login?message=" + error.message);
     }
 
     return redirect(data.url);
@@ -65,7 +65,7 @@ export default function Login({
     });
 
     if (error) {
-      return redirect("/login?message=Could not authenticate user");
+      return redirect("/login?message=" + error.message);
     }
 
     return redirect(data.url);
@@ -85,7 +85,7 @@ export default function Login({
     });
 
     if (error) {
-      return redirect("/login?message=Could not authenticate user");
+      return redirect("/login?message=" + error.message);
     }
 
     return redirect(data.url);
