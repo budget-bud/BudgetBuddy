973fc979b78fc98051134c1d74f29bca4bb2dedf
AlexMilos <11dmilosa@gmail.com>
2023-12-16 15:10:32 +0100

    feat: implemented mock chat apis

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,4 +1,6 @@
+import { isNullOrUndefined } from "@/utils/isNullOrUndefined";
 import { ChatOpenAI } from "langchain/chat_models/openai";
+import { AIMessage } from "langchain/dist/schema";
 import { PromptTemplate } from "langchain/prompts";
 import { NextRequest, NextResponse } from "next/server";
 
@@ -13,14 +15,38 @@ Question: {question}`;
 export async function POST(req: NextRequest) {
   try {
     const body = await req.json();
+    if (isNullOrUndefined(body.question)) {
+      return NextResponse.json(
+        { error: "Please provide a question" },
+        { status: 400 },
+      );
+    }
+
+    if (body.chatId === undefined) {
+      return NextResponse.json(
+        { error: "Please provide a chatId" },
+        { status: 400 },
+      );
+    }
+
     const chat = new ChatOpenAI({});
     const prompt = PromptTemplate.fromTemplate(PROMPT_TEMPLATE);
     const runnable = prompt.pipe(chat);
-    const response = await runnable.invoke({
+    const response: AIMessage = await runnable.invoke({
       question: body.question,
     });
 
-    return NextResponse.json(response, { status: 200 });
+    const answer = response.lc_kwargs.content;
+
+    console.log(response.lc_kwargs.content);
+
+    return NextResponse.json(
+      {
+        answer,
+        chatId: body.chatId,
+      },
+      { status: 200 },
+    );
   } catch (e) {
     return NextResponse.json({ error: (e as Error).message }, { status: 500 });
   }
