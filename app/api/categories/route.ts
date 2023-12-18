c0e62b66ee7163694ea23e529d1a7056c4f69ee8
Kálmán Neszlényi <73647069+kamka427@users.noreply.github.com>
2023-12-18 02:08:31 +0100

    fix: category transaction calculation

--- app/api/categories/route.ts
+++ app/api/categories/route.ts
@@ -80,11 +80,34 @@ export async function POST(req: NextRequest) {
       ])
       .select("*");
 
+      const categoriesWithT = await Promise.all(
+        categories?.map(async (category) => {
+          const { data: transactions, error: transactionsError } = await supabase
+            .from("Transactions")
+            .select("movement")
+            .eq("user_id", user?.id)
+            .eq("category_id", category.id);
+  
+          const totalAmount = transactions?.reduce((acc, curr) => {
+            return acc + curr.movement;
+          }, 0);
+  
+          if (transactionsError) {
+            return NextResponse.json(
+              { error: transactionsError.message },
+              { status: 500 },
+            );
+          }
+  
+          return { ...category, totalAmount };
+        }) || [],
+      );
+
     if (error) {
       return NextResponse.json({ error: error.message }, { status: 500 });
     }
 
-    return NextResponse.json({ categories }, { status: 200 });
+    return NextResponse.json({ categories: categoriesWithT }, { status: 200 });
   } catch (e) {
     return NextResponse.json({ error: (e as Error).message }, { status: 500 });
   }
