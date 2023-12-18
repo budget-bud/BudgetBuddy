7c6bf3420d7ca23191cdabcf2d0d244b8949aba1
Kálmán Neszlényi <73647069+kamka427@users.noreply.github.com>
2023-12-18 18:02:45 +0100

    feat: added pulse placeholder

--- components/Chat.tsx
+++ components/Chat.tsx
@@ -11,14 +11,24 @@ const Chat = () => {
   const pathname = usePathname();
   const chatId = pathname.split("/")[2];
 
+  const initialMessages: IMessage[] = [
+    {
+      content: "Hello! :) \nHow can I help you today!\nI can query all of your data stored in the app.",
+      role: "assistant",
+    },
+  ]
+
   const [input, setInput] = useState<string>("");
-  const [messages, setMessages] = useState<IMessage[]>([]);
+  const [messages, setMessages] = useState<IMessage[]>(initialMessages);
+  const [isResponding, setIsResponding] = useState<boolean>(false);
 
   const sendMessage = async (): Promise<void> => {
     if (input.length === 0) return;
 
     setMessages([...messages, { content: input, role: "user" }]);
     setInput("");
+
+    setIsResponding(true);
 
     const response = await fetch(`/api/chat`, {
       method: "POST",
@@ -32,6 +42,8 @@ const Chat = () => {
       window.alert(response.error);
       return;
     }
+
+    setIsResponding(false);
 
     setMessages([
       ...messages,
@@ -61,8 +73,9 @@ const Chat = () => {
     <section className="flex max-h-full w-full flex-grow flex-col gap-4 rounded-lg bg-secondary-800 p-4">
       <div className="scrollbar flex max-h-full w-full flex-grow flex-col gap-3 overflow-y-scroll p-3">
         {messages.map((message, index) => (
-          <Message key={index} message={message} />
+          <Message key={index} message={message} isResponding={isResponding}/>
         ))}
+        {isResponding && <RespondPlaceholder />}
       </div>
       <div className="h-[100px] w-full">
         <div className="flex h-full w-full flex-row items-center justify-center gap-4 rounded-[18px] bg-secondary-800 p-4 text-lg text-text-100 md:w-full">
@@ -87,7 +100,7 @@ const Chat = () => {
 
 export default Chat;
 
-const Message = ({ message }: { message: IMessage }) => {
+const Message = ({ message }: { message: IMessage, isResponding: boolean }) => {
   if (message.role == "user")
     return (
       <div className="mb-[10px] flex items-center justify-end gap-[10px]">
@@ -105,3 +118,13 @@ const Message = ({ message }: { message: IMessage }) => {
       </div>
     );
 };
+
+const RespondPlaceholder = () => {
+  return (
+    <div className="mb-[10px] flex items-center justify-start gap-[10px]">
+      <p className="animate-pulse whitespace-pre-wrap rounded-bl-[7px] rounded-br-[18px] rounded-tl-[18px] rounded-tr-[18px] bg-secondary-500 p-[10px]">
+        I'm thinking...
+      </p>
+    </div>
+  );
+}
